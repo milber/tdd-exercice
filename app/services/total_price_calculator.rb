af6cdafc451b964de2795f7260bb191dcf5940af
Milber Champutiz Burbano <milber.champutiz@thoughtworks.com>
2024-07-01 17:20:04 -0500

    feat: create calculate price service and test skeletton

---
+++ app/services/total_price_calculator.rb
@@ -0,0 +1,12 @@
+class TotalPriceCalculator
+
+    def initialize(number_of_tickets, unitary_ticket_price)
+        @number_of_tickets = number_of_tickets
+        @unitary_ticket_price = unitary_ticket_price
+    end
+    
+    def call
+        return nil
+    end
+
+end
---
+++ spec/services/TotalPriceCalculatorSpec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe TotalPriceCalculator, type: :model do
+
+    describe '#call' do
+
+        # TO-DO:  write the tests
+
+    end
+
+end
