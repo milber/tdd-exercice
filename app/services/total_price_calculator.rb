6732bea4f836ce7022e0725a8b15a79444d75a90
Milber Champutiz Burbano <milber.champutiz@thoughtworks.com>
2024-07-03 09:32:55 -0500

    test: reset all

--- app/services/total_price_calculator.rb
+++ app/services/total_price_calculator.rb
@@ -6,20 +6,6 @@ class TotalPriceCalculator
     end
     
     def call
-        total_price = @number_of_tickets * @ticket_unit_price - discount
-        return total_price
+        
     end
-
-    private
-    def discount
-        discount_unit_value = 2
-        discount_block = 5
-
-        factor_discount = @number_of_tickets / discount_block
-
-        discount = discount_unit_value * factor_discount
-
-        return discount
-    end
-
 end
--- spec/services/TotalPriceCalculatorSpec.rb
+++ spec/services/TotalPriceCalculatorSpec.rb
@@ -3,63 +3,7 @@ require 'rails_helper'
 RSpec.describe TotalPriceCalculator, type: :model do
 
     describe '#call' do
-
-        it 'calculate the price of 1 ticket' do
-            # arrange
-            number_of_tickets = 1
-            ticket_unit_price = 2
-            expected_total_cost = 2
-
-            # act
-            calculator = TotalPriceCalculator.new(number_of_tickets, ticket_unit_price)
-            calculated_total_price = calculator.call
-
-            # assert
-            expect(calculated_total_price).to eq(expected_total_cost)
-        end
-
-        it 'calculate the price of 4 ticket' do
-            # arrange
-            number_of_tickets = 4
-            ticket_unit_price = 2
-            expected_total_cost = 8
-
-            # act
-            calculator = TotalPriceCalculator.new(number_of_tickets, ticket_unit_price)
-            calculated_total_price = calculator.call
-
-            # assert
-            expect(calculated_total_price).to eq(expected_total_cost)
-        end
-
-        it 'calculate the price of 5 ticket' do
-            # arrange
-            number_of_tickets = 5
-            ticket_unit_price = 2
-            expected_total_cost = 8
-
-            # act
-            calculator = TotalPriceCalculator.new(number_of_tickets, ticket_unit_price)
-            calculated_total_price = calculator.call
-
-            # assert
-            expect(calculated_total_price).to eq(expected_total_cost)
-        end
-
-        it 'calculate the price of 5 ticket' do
-            # arrange
-            number_of_tickets = 7
-            ticket_unit_price = 2
-            expected_total_cost = 12
-
-            # act
-            calculator = TotalPriceCalculator.new(number_of_tickets, ticket_unit_price)
-            calculated_total_price = calculator.call
-
-            # assert
-            expect(calculated_total_price).to eq(expected_total_cost)
-        end
-
+        # TO-DO: write tests
     end
 
 end
